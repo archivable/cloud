5f367a8f9292f3d814705af6b3e4578649591001
vaux <zero.griffin@gmail.com>
2021-03-16 16:13:22 +0100

    Add date

---
+++ Sources/Date.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension Date {
+    public init(timestamp: UInt32) {
+        self.init(timeIntervalSince1970: .init(timestamp))
+    }
+    
+    public var timestamp: UInt32 {
+        .init(timeIntervalSince1970)
+    }
+}
