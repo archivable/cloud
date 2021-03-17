99a2c9f32842cc5abc252824627f6b5cc761552e
vaux <zero.griffin@gmail.com>
2021-03-17 10:51:40 +0100

    Add array extension

---
+++ Sources/Array.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+extension Array {
+    public func mutating(index: Int, transform: (Element) -> Element) -> Self {
+        var array = self
+        array[index] = transform(array[index])
+        return array
+    }
+    
+    public func moving(from: Int, to: Int) -> Self {
+        var array = self
+        array.insert(array.remove(at: from), at: Swift.min(to, array.count))
+        return array
+    }
+    
+    public func removing(index: Int) -> Self {
+        var array = self
+        array.remove(at: index)
+        return array
+    }
+    
+    public static func +(array: Self, element: Element) -> Self {
+        var array = array
+        array.append(element)
+        return array
+    }
+    
+    public static func +(element: Element, array: Self) -> Self {
+        var array = array
+        array.insert(element, at: 0)
+        return array
+    }
+}
