07291a97cf51e7c3d7850b6ec67efa2d39ded0ca
vaux <zero.griffin@gmail.com>
2021-09-01 13:02:50 +0200

    Add set removing

--- Sources/Set.swift
+++ Sources/Set.swift
@@ -6,4 +6,14 @@ extension Set {
         set.insert(element)
         return set
     }
+    
+    public func removing(_ element: Element) -> Self {
+        var set = self
+        _ = set
+            .firstIndex(of: element)
+            .map {
+                set.remove(at: $0)
+            }
+        return set
+    }
 }
