b0feed4a329b28e14cf39d5279e468a9e717d9dd
vaux <zero.griffin@gmail.com>
2021-10-02 17:58:58 +0200

    Update parameters

--- Sources/Data.swift
+++ Sources/Data.swift
@@ -43,29 +43,29 @@ extension Data {
         return transform(&mutating)
     }
     
-    public mutating func collection<I, S>(_ size: I.Type) -> [S] where I : UnsignedInteger, S : Storable {
+    public mutating func collection<I, S>(size: I.Type) -> [S] where I : UnsignedInteger, S : Storable {
         (0 ..< .init(number() as I))
             .map { _ in
                 .init(data: &self)
             }
     }
     
-    public mutating func collection<I, J>(_ collection: I.Type, _ strings: J.Type) -> [String] where I : UnsignedInteger, J : UnsignedInteger {
+    public mutating func items<I, J>(collection: I.Type, strings: J.Type) -> [String] where I : UnsignedInteger, J : UnsignedInteger {
         (0 ..< .init(number() as I))
             .map { _ in
-                string(strings)
+                string(size: strings)
             }
     }
     
-    public mutating func unwrap<I>(_ size: I.Type) -> Data where I : UnsignedInteger {
+    public mutating func unwrap<I>(size: I.Type) -> Data where I : UnsignedInteger {
         let size = Int(number() as I)
         let result = subdata(in: 0 ..< size)
         self = removing(size)
         return result
     }
     
-    public mutating func string<I>(_ size: I.Type) -> String where I : UnsignedInteger {
-        .init(decoding: unwrap(size), as: UTF8.self)
+    public mutating func string<I>(size: I.Type) -> String where I : UnsignedInteger {
+        .init(decoding: unwrap(size: size), as: UTF8.self)
     }
     
     public mutating func date() -> Date {
@@ -73,7 +73,7 @@ extension Data {
     }
     
     public mutating func uuid() -> UUID {
-        UUID(uuidString: string(UInt8.self))!
+        UUID(uuidString: string(size: UInt8.self))!
     }
     
     public mutating func bool() -> Bool {
@@ -92,16 +92,16 @@ extension Data {
         self + storable.data
     }
     
-    public func adding<I, S>(_ size: I.Type, collection: [S]) -> Self where I : UnsignedInteger, S : Storable {
+    public func adding<I, S>(size: I.Type, collection: [S]) -> Self where I : UnsignedInteger, S : Storable {
         adding(I(collection.count))
             .adding(collection.flatMap(\.data))
     }
     
-    public func adding<I, J>(_ collection: I.Type, strings: J.Type, items: [String]) -> Self where I : UnsignedInteger, J : UnsignedInteger {
+    public func adding<I, J>(collection: I.Type, strings: J.Type, items: [String]) -> Self where I : UnsignedInteger, J : UnsignedInteger {
         items
             .reduce(adding(I(items.count))) {
                 $0
-                    .adding(strings, string: $1)
+                    .adding(size: strings, string: $1)
             }
     }
     
@@ -113,12 +113,12 @@ extension Data {
         self + collection
     }
     
-    public func wrapping<I>(_ size: I.Type, data: Data) -> Self where I : UnsignedInteger {
+    public func wrapping<I>(size: I.Type, data: Data) -> Self where I : UnsignedInteger {
         adding(I(data.count)) + data
     }
     
-    public func adding<I>(_ size: I.Type, string: String) -> Self where I : UnsignedInteger {
-        wrapping(size, data: .init(string.utf8))
+    public func adding<I>(size: I.Type, string: String) -> Self where I : UnsignedInteger {
+        wrapping(size: size, data: .init(string.utf8))
     }
     
     public func adding(_ date: Date) -> Self {
@@ -126,7 +126,7 @@ extension Data {
     }
     
     public func adding(_ uuid: UUID) -> Self {
-        adding(UInt8.self, string: uuid.uuidString)
+        adding(size: UInt8.self, string: uuid.uuidString)
     }
     
     public func adding(_ bool: Bool) -> Self {
